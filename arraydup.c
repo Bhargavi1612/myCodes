cb6ef706dd316d9da5a575ef77e92bba1d0dcd9e
Bhargavi1612 <100599898+Bhargavi1612@users.noreply.github.com>
2022-04-12 18:31:38 +0530

    Create arraydup.c

---
+++ arraydup.c
@@ -0,0 +1,51 @@
+// Online C compiler to run C program online
+#include <stdio.h>  
+int delete_duplicate(int array[], int n)  
+{  
+ int ret;  
+// Please add your code Here  
+
+if(n==0||n==1)
+return  n;
+int temp[n];
+int i,j=0;
+for(i=0;i<n-1;i++)
+{
+if(array[i]!=array[i+1])
+temp[j++]=array[i];
+}
+temp[j++]=array[n-1];
+for(i=0;i<j;i++)
+{
+array[i]=temp[i];
+}
+ return j;
+}  
+int main()  
+{  
+ int n;  
+ printf("\n Enter the array size ");  
+ scanf("%d", &n);  
+ int array[n];  
+ int i;  
+ printf("\n Enter the array values ");  
+ for (i = 0; i < n; i++)  
+ {  
+ scanf("%d", &array[i]);  
+ }  
+ printf("\nArray value before removing duplicate ");  
+ // Please add your code Here  
+for(i=0;i<n;i++)
+{
+printf("%d ",array[i]);
+}
+ n = delete_duplicate(array, n); 
+ //print the array without duplicates  
+ // Please add your code Here 
+printf("\nArray after removing duplicates:");
+for(i=0;i<n;i++)
+{
+printf("%d ",array[i]);
+} 
+ return 0;  
+}
